e7c145af4afd3c44faab012974c8867136396ddb
Gloria <izayura@gmail.com>
2020-08-08 13:48:42 -0500

    Front-end User Story 1 -Install vue module -Create folder "components" inside folder "app" -Create App.vue inside "components" -Configurate index.js inside "app" to receive code from App.vue -Write <template> and <script> inside App.vue to visualize the items from my database -Run command 'npm run webpack' to update bundle.js and visualize the changes I made

---
+++ src/app/components/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="container-fluid bg-dark text-white text-center py-3">
+            <div class="container">
+                <h1>Shopping Cart Interface</h1>
+            </div>
+        </div>
+        <div class="container-fluid  py-3">
+            <div class="container">
+                <div class="row">
+                    <div v-for="item of items" class="col col-sm-6 col-md-4 col-lg-3 col-xl-2 border" >
+                        <section class="py-3">
+                            <img v-bind:src="item.image" v-bind:alt="item.image" width="100" height="100">
+                            <p>{{item.title}}</p>
+                            <p>${{item.price}}</p>
+                        </section>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            items:[]
+        }
+    },
+    created(){
+        this.displayAllItems();
+    },
+    methods:{
+        displayAllItems(){
+            console.log("displayAllItems");
+            fetch('/api/item')
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                this.items=data;
+                console.log(this.items);
+            })
+        }
+    }
+}
+</script>
